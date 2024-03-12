9a0a3479b397206db2ee9e87f074169c27008bac
Irineu333 <mmmirineusilva@gmail.com>
2024-03-11 23:08:47 -0300

    Add --version option

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,18 @@
 package org.example;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Arrays.asList(args));
+
+        if (args.length == 0) {
+            System.out.println("No command provided");
+            return;
+        }
+
+        String command = args[0];
+
+        if(command.equals("--version")) {
+            System.out.println("1.0-DEV");
+            return;
+        }
     }
 }
